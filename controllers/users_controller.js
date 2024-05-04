c72f8c571bcd2e5c1995729389bfed03599c1cce
prakashMali1527 <prakash.dakshana@gmail.com>
2024-05-04 16:57:58 +0530

    add Partials

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,11 +1,11 @@
-module.exports.profile = function(req, res) {
-    res.end('<h1> User Profile</h1>');
+module.exports.profile = function (req, res) {
+    res.render('user_profile', { title: 'profile' });
 }
 
-module.exports.post = function(req,res){
+module.exports.post = function (req, res) {
     res.end('<h1>User Post</h1>');
 }
 
-module.exports.friends = function(req,res){
+module.exports.friends = function (req, res) {
     res.send('<ul> <li> friend List </li> </ul>');
 }
