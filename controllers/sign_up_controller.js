7adcb22e71efa77c0aba1b0114d2b92003ec7ef4
prakashMali1527 <prakash.dakshana@gmail.com>
2024-05-05 17:38:49 +0530

    Get the Sign Up data successfully to database

--- controllers/sign_up_controller.js
+++ controllers/sign_up_controller.js
@@ -1,10 +1,34 @@
-module.exports.signUp = function(req,res){
+const User = require('../models/user');
+module.exports.signUp = function (req, res) {
     res.render('signup');
 }
 
 // get the sign up data
-module.exports.createUser = function(req,res){
-    console.log('user created');
-    console.log(req.body);
-    res.redirect('back');
+module.exports.createUser = function (req, res) {
+    const { name, email, password, confirm_password } = req.body;
+
+    if (password != confirm_password) {
+        console.log('Password and Confirm Password are different.Try again!!')
+        return res.redirect('back');
+    }
+
+    User.findOne({email})
+    .then(function (user) {
+        if (user) {
+            console.log('User already exist');
+            return res.redirect('back');
+        }
+
+        User.create({ name, email, password })
+        .then(function (newUser) {
+            res.redirect('/signin');
+        }).catch(function(err) {
+            console.log('Error in creating User while Signing Up');
+            return;
+        });
+
+    }).catch(function(err) {
+        console.log('Error finding the User in Signing Up');
+        return;
+    });
 }
