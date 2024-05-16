02963dcea0c5d22db48a136d5436bbf2fbb8b9a3
prakashMali1527 <prakash.dakshana@gmail.com>
2024-05-16 17:22:05 +0530

    Replace avatar if already exist

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const fs = require('fs');
+const path = require('path');
 
 module.exports.profile = function (req, res) {
 
@@ -28,6 +30,15 @@ module.exports.update = async function(req,res){
                 user.email = req.body.email;
 
                 if(req.file){
+
+                    if(user.avatar){
+                        try{
+                            fs.unlinkSync(path.join(__dirname,'..',user.avatar));
+                        }catch(err){
+                            console.log(`Error: ${err}`);
+                        }
+                    }
+
                     // saving the path of the uploaded file into the avatar field in the user
                     user.avatar = User.avatarPath + '/' + req.file.filename;
                 }
