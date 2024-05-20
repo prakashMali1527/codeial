67c5177ee3c9fc70069782de65a82bdf54c16fca
prakashMali1527 <prakash.dakshana@gmail.com>
2024-05-20 13:31:03 +0530

    Add google auth and Google SignIn/signUp

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const generator = require('generate-password');
+
+const User = require('../models/user');
+
+// tell passport to use a new strategy for google login
+passport.use(new googleStrategy({
+        clientID: "",
+        clientSecret: "",
+        callbackURL: ""
+    },
+    async function(accessToken, refreshToken, profile, done){
+        try{
+            console.log(profile);
+            // find a user
+            let user = await User.findOne({email: profile.emails[0].value});
+            if(user){
+                // if found, set this user as req.user
+                return done(null, user);
+            }else{
+                try{
+                    // if not found, create the user and set it as req.user
+                    let user = User.create({
+                        name: profile.displayName,
+                        email: profile.emails[0].value,
+                        password: generator.generate({
+                            length: 20,
+                            numbers: true
+                        })
+                    })
+                }catch(err){
+                    console.log('Error in creating user, google passport-strategy ',err);
+                }
+            }
+        }catch(err){
+            console.log('Error in google passport-strategy ',err);
+        }
+    }
+));
+
+module.exports = passport;
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const app = express();
 const usersController = require('../controllers/users_controller');
+const signInController = require('../controllers/sign_in_controller');
 const passport = require('passport');
 
 router.get('/profile/:id', passport.checkAuthentication, usersController.profile);
@@ -9,5 +10,8 @@ router.post('/update/:id', passport.checkAuthentication, usersController.update)
 router.get('/posts', usersController.posts);
 router.get('/friends', usersController.friends);
 router.get('/destroy-session', usersController.destroySession);
+
+router.get('/auth/google',passport.authenticate('google',{scope: ['profile','email']}));
+router.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/signIn'}),signInController.createSession);
 
 module.exports = router;
